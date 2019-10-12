1f1ea916d33d08ed994bc0e66e148b8535bc382d
shristishandilya <shristishandilya2103@outlook.com>
2019-10-12 17:16:05 +0530

    pdp page generated, header new code mergered
    Signed-off-by: shristishandilya <shristishandilya2103@outlook.com>

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,38 +1,38 @@
 $(document).ready(function(){
+
+  // Top banner/jumbotron carousel
   $('.top-carousel').slick({
     arrows: false,
+    dots: true,
   });
 
 
-    $('.top-books-carousel').slick({
-        // arrows: false,
-        responsive: [
-            {
-                breakpoint: 4000,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                }
-            },
-            {
-                breakpoint: 385,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ]
-    });
-
+  // Top deals/books carousel
+  $('.top-books-carousel').slick({
+    // arrows: false,
+    responsive: [
+      {
+        breakpoint: 4000,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 385,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });
 
 });
