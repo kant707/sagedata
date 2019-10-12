f1930b6a096221b79b9cc994be6e9316892bce4a
shristishandilya <shristishandilya2103@outlook.com>
2019-10-12 21:58:25 +0530

    slider added product page
    Signed-off-by: shristishandilya <shristishandilya2103@outlook.com>

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,38 +1,86 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-  // Top banner/jumbotron carousel
-  $('.top-carousel').slick({
-    arrows: false,
-    dots: true,
-  });
+    // Top banner/jumbotron carousel
+    $('.top-carousel').slick({
+        arrows: false,
+        dots: true,
+    });
 
 
-  // Top deals/books carousel
-  $('.top-books-carousel').slick({
-    // arrows: false,
-    responsive: [
-      {
-        breakpoint: 4000,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        }
-      },
-      {
-        breakpoint: 385,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        }
-      }
-    ]
-  });
+    // Top deals/books carousel
+    $('.top-books-carousel').slick({
+        // arrows: false,
+        responsive: [
+            {
+                breakpoint: 4000,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 385,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+
+//  pdd sliders
+    $('.products-slider').slick({
+        arrows: true,
+        dots: false,
+        asNavFor: '.products-slider',
+        infinite: false,
+        speed: 300,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        responsive: [
+            {
+                breakpoint: 1139,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 5,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4
+                }
+            }, {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }, {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+        ]
+    });
 
 });
