99570128cb975a94dbb4bc36b7df6b4f4656f0fb
SHASHIKANT <kant707@gmail.com>
2019-10-14 00:40:04 +0530

    1. product page initialized. 2. product description added and some part of responsiveness completed. 3. editor pick and customer's choice carousel added.

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -35,4 +35,97 @@ $(document).ready(function(){
     ]
   });
 
+
+  // Editor's pick and Customer's choice books carousel
+  $('.editors-pick-carousel, .customer-choice-carousel').slick({
+    // arrows: false,
+    responsive: [
+      {
+        breakpoint: 4000,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 390,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      }
+    ]
+  });
+
+
+
+  //  pdp sliders
+  $('.products-slider').slick({
+    arrows: true,
+    dots: false,
+    asNavFor: '.products-slider',
+    infinite: false,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    responsive: [
+      {
+        breakpoint: 1139,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4
+        }
+      }, {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }, {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+    ]
+  });
+
 });
